3389e7b456938da89f5a39c637216a5b39e5d460
mjdownes <molly@tritonehealth.com>
2025-05-29 18:48:52 +0100

    fix(UI): changed nav bar to orange and white, with labels and coloured icons

--- src/Templates/NavBar.tsx
+++ src/Templates/NavBar.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import Menstruation from '../assets/Navigation/menstruation-cycle.png';
-import Menopause from '../assets/Navigation/menopause.png';
-import History from '../assets/Navigation/history.png';
-import Learning from '../assets/Navigation/idea.png';
+import Menstruation from '../assets/v2/Navigation/menstruation.svg';
+import Menopause from '../assets/v2/Navigation/menopause.svg';
+import History from '../assets/v2/Navigation/rewind.svg';
+import Learning from '../assets/v2/Navigation/learning.svg';
 import { twMerge } from 'tailwind-merge';
 import { useNavigate } from 'react-router-dom';
 
@@ -16,37 +16,41 @@ export default function NavBar({ current }: NavBarProps) {
     const navigate = useNavigate()
 
     return (
-        <div className="flex w-full border-t border-pink-400">
+        <div className="flex w-full border-t border-zinc-700">
 
             <button
-                className={twMerge(`flex flex-1 items-center justify-center border-r border-pink-400 py-6`,
-                    selected === "menstruation" ? "bg-red-300 inset-shadow-sm inset-shadow-red-400" : "bg-rose-50")}
+                className={twMerge(`flex flex-col items-center justify-center border-r w-[25%] border-zinc-700 py-3`,
+                    selected === "menstruation" ? "bg-orange-200 inset-shadow-sm inset-shadow-orange-300" : "bg-white")}
                 onClick={() => { setSelected("menstruation"); navigate('/') }}> {/*Styling for when a button is clicked */}
                 <img src={Menstruation} className='w-[40%] h-auto' />
+                <p className="mb-2"> Periods</p>
             </button>
 
 
             <button
-                className={twMerge(`flex flex-1 items-center justify-center border-r border-pink-400 py-6`,
-                    selected === "menopause" ? "bg-red-300 inset-shadow-sm inset-shadow-red-400" : "bg-rose-50")}
+                className={twMerge(`flex flex-col items-center justify-center border-r w-[25%] border-zinc-700 py-3`,
+                    selected === "menopause" ? "bg-orange-200 inset-shadow-sm inset-shadow-orange-300" : "bg-white")}
                 onClick={() => { setSelected("menopause"); navigate('/menopause') }}>
                 <img src={Menopause} className='w-[40%] h-auto' />
+                <p className="mb-2"> Menopause </p>
             </button>
 
 
             <button
-                className={twMerge(`flex flex-1 items-center justify-center border-r border-pink-400 py-6`,
-                    selected === "history" ? "bg-red-300 inset-shadow-sm inset-shadow-red-400" : "bg-rose-50")}
+                className={twMerge(`flex flex-col items-center justify-center border-r w-[25%] border-zinc-700 py-3`,
+                    selected === "history" ? "bg-orange-200 inset-shadow-sm inset-shadow-orange-300" : "bg-white")}
                 onClick={() => { setSelected("history"); navigate('/history') }}>
                 <img src={History} className='w-[40%] h-auto' />
+                <p className="mb-2"> History </p>
             </button>
 
 
             <button
-                className={twMerge(`flex flex-1 items-center justify-center border-r border-pink-400 py-6`,
-                    selected === "learning" ? "bg-red-300 inset-shadow-sm inset-shadow-red-400" : "bg-rose-50")}
+                className={twMerge(`flex flex-col items-center justify-center border-r w-[25%] border-zinc-700 py-3`,
+                    selected === "learning" ? "bg-orange-200 inset-shadow-sm inset-shadow-orange-300" : "bg-white")}
                 onClick={() => { setSelected("learning"); navigate('/learning') }}>
                 <img src={Learning} className='w-[40%] h-auto' />
+                <p className="mb-2"> Learning</p>
             </button>
         </div>
     )
